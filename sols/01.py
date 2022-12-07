f355b8907113320b225362b62c5eeae5b08d87ae
Tyler Lin <tylerhlin@gmail.com>
2022-12-06 22:50:54 -0500

    Day 1 complete

---
+++ sols/01.py
@@ -0,0 +1,5 @@
+input = open('inputs/01.txt').read()
+calories = sorted([sum([int(count) for count in inventory.split('\n')])
+                   for inventory in input.split('\n\n')], reverse=True)
+print(calories[0])
+print(calories[0] + calories[1] + calories[2])
