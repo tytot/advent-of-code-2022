15cc94bb06ac715ed0b4d557cf400ff76946f245
Tyler Lin <tylerhlin@gmail.com>
2022-12-11 17:51:24 -0500

    Day 5 complete

---
+++ 05/solution.py
@@ -0,0 +1,16 @@
+input = open(__file__.rstrip('solution.py') + 'input.txt').read()
+crates, steps = [lines.split('\n') for lines in input.split('\n\n')]
+stacks = [[line[i] for line in ([line[i + 1] for i in range(0, len(line), 4)] for line in crates[:-1]) if line[i].strip()][::-1] for i in range(int(crates[-1][-2]))]
+steps = [[int(num) for num in step.split(' ')[1::2]] for step in steps]
+
+stackscp = [stack[:] for stack in stacks]
+for amount, frm, to in steps:
+    stackscp[to - 1].extend(stackscp[frm - 1][-amount:][::-1])
+    del stackscp[frm - 1][-amount:]
+print(''.join([stack[-1] for stack in stackscp]))
+
+stackscp = [stack[:] for stack in stacks]
+for amount, frm, to in steps:
+    stackscp[to - 1].extend(stackscp[frm - 1][-amount:])
+    del stackscp[frm - 1][-amount:]
+print(''.join([stack[-1] for stack in stackscp]))
